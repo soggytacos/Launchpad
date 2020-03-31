aa798ae02819a4432c9da8570dcc54df445ba0b4
Colman <colman.swisher@gmail.com>
2020-03-31 16:24:44 -0500

    added a date picker so the user can save the due date of the todo.

--- src/containers/ToDo/ToDo.js
+++ src/containers/ToDo/ToDo.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
 import classes from './ToDo.css';
 import ToDoList from './ToDoList/ToDoList';
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css'
+import PropTypes from 'prop-types';
+import setHours from "date-fns/setMinutes";
+import setMinutes from "date-fns/setMinutes";
 
 library.add(faTrash);
 
@@ -10,38 +15,48 @@ class ToDo extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            selectedDate: '',
             items: [],
             currentItem: {
                 text: '',
                 key: '',
+                dueDate: ''
             }
         }
         this.handleInput = this.handleInput.bind(this);
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
         this.setUpdate = this.setUpdate.bind(this);
+        this.handleDueDate = this.handleDueDate.bind(this);
+        this.changeDueDate = this.changeDueDate.bind(this);
     }
 
     handleInput(e) {
-        let now = new Date().toLocaleDateString("en-US");
-        this.setState({
-            currentItem: {
-                text: e.target.value,
-                key: now
-            }
-        })
+        let now = Date.now();
+        const newItem = {...this.state.currentItem}
+        newItem.text = e.target.value;
+        newItem.key = now;
+        this.setState({currentItem: newItem})
+    }
+
+    handleDueDate = (date) => {
+        const newItem = {...this.state.currentItem}
+        newItem.dueDate = date;
+        this.setState({currentItem: newItem})
     }
 
     addItem(e) {
         e.preventDefault();
         const newItem = this.state.currentItem;
-        if(newItem.text !== "") {
+        if (newItem.text !== "") {
             const newItems = [...this.state.items, newItem];
             this.setState({
                 items: newItems,
+                selectedDate: '',
                 currentItem: {
                     text: '',
-                    key: ''
+                    key: '',
+                    dueDate: ''
                 }
             })
         }
@@ -49,7 +64,7 @@ class ToDo extends Component {
 
     deleteItem(key) {
         const filteredItems = this.state.items.filter(item => item.key !== key);
-        this.setState({ items: filteredItems })
+        this.setState({items: filteredItems})
     }
 
     setUpdate(text, key) {
@@ -60,22 +75,51 @@ class ToDo extends Component {
                 }
             }
         )
-        this.setState({ items: items })
+        this.setState({items: items})
+    }
+
+    changeDueDate(date, key) {
+        const items = this.state.items;
+        items.map(item => {
+                if (item.key === key) {
+                    item.dueDate = date;
+                }
+            }
+        )
+        this.setState({items: items})
     }
 
     render() {
         return (
-            <div className={classes.ToDoForm}>
-                <form id="to-do-form" onSubmit={this.addItem}>
-                    <input className={classes.Input} type="text" placeholder="Enter Text"
-                    value={this.state.currentItem.text}
-                    onChange={this.handleInput}/>
-                    <button className={classes.button} type="submit">+</button>
-                </form>
+            <div>
+                <div className={classes.ToDoForm}>
+                    <DatePicker className={classes.DatePicker}
+                        onChange={date => this.handleDueDate(date)}
+                        selected={this.state.currentItem.dueDate}
+                        showTimeSelect
+                        excludeTimes={[
+                            setHours(setMinutes(new Date(), 0), 17),
+                            setHours(setMinutes(new Date(), 30), 18),
+                            setHours(setMinutes(new Date(), 30), 19),
+                            setHours(setMinutes(new Date(), 30), 17)
+                        ]}
+                        dateFormat="MMM d, yyyy h:mm aa"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter Text"
+                        value={this.state.currentItem.text}
+                        onChange={this.handleInput}
+                    />
+                    <span>
+                        <button className={classes.Button} onClick={this.addItem} type="submit">+</button>
+                    </span>
+                </div>
                 <ToDoList
                     items={this.state.items}
                     deleteItem={this.deleteItem}
                     setUpdate={this.setUpdate}
+                    changeDueDate={this.changeDueDate}
                 />
             </div>
         );
--- src/containers/ToDo/ToDoList/ToDoList.js
+++ src/containers/ToDo/ToDoList/ToDoList.js
@@ -2,29 +2,43 @@ import React from 'react';
 import classes from './ToDoList.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import FlipMove from "react-flip-move";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css'
+import PropTypes from 'prop-types';
+import setHours from "date-fns/setMinutes";
+import setMinutes from "date-fns/setMinutes";
 
 const toDoList = (props) => {
     const items = props.items;
     let listItems = items.map(item => (
         <div key={item.key} className={classes.ToDoItem}>
-            <p>
-                <input
-                    type="text"
-                    id={item.key}
-                    value={item.text}
-                    onChange={(e) => {
-                        props.setUpdate(e.target.value, item.key)
-                    }}
+                <DatePicker
+                    onChange={(date) => props.changeDueDate(date, item.key)}
+                    selected={item.dueDate}
+                    showTimeSelect
+                    excludeTimes={[
+                        setHours(setMinutes(new Date(), 0), 17),
+                        setHours(setMinutes(new Date(), 30), 18),
+                        setHours(setMinutes(new Date(), 30), 19),
+                        setHours(setMinutes(new Date(), 30), 17)
+                    ]}
+                    dateFormat="MMM d, yyyy h:mm aa"
                 />
-                <span>
-                    {item.key}
+                    <input
+                        type="text"
+                        id={item.key}
+                        value={item.text}
+                        onChange={(e) => {
+                            props.setUpdate(e.target.value, item.key)
+                        }}
+                    />
+                    <span>
                     <FontAwesomeIcon
                         icon="trash"
                         onClick={() => props.deleteItem(item.key)}
                         className={classes.Faicons}
                     />
                 </span>
-            </p>
         </div>
     ))
 
