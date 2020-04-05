727c31790c43ef47bf72eae33dd051da528bed10
Colman <colman.swisher@gmail.com>
2020-04-05 10:32:54 -0500

    hardcoded the morning journal promps and added the submit functionality.

--- src/containers/Launchpad/Launchpad.js
+++ src/containers/Launchpad/Launchpad.js
@@ -4,6 +4,7 @@ import MorningJournal from "../MorningJournal/MorningJournal";
 import Button from '../../components/UI/Button/Button';
 import Quotes from "../Quotes/Quotes";
 import ToDo from '../ToDo/ToDo';
+import Prompts from '../MorningJournal/Prompts/Propmpts';
 
 class Launchpad extends Component {
 
@@ -20,6 +21,7 @@ class Launchpad extends Component {
                 <Quotes />
                 <MorningJournal />
                 <ToDo />
+                <Prompts />
             </Aux>
         )
     }
---
+++ src/containers/MorningJournal/Prompts/Propmpts.js
@@ -0,0 +1,113 @@
+import React, {Component} from 'react';
+
+class Prompts extends Component {
+
+    state = {
+        prompts: {
+            prompt1: {
+                question: "3 things that I'm grateful for today are...",
+                answer: '',
+                timestamp: ''
+            },
+            prompt2: {
+                question: "I am...",
+                answer: '',
+                timestamp: ''
+            },
+            prompt3: {
+                question: "A situation that might stress me out or trip me up today is...",
+                answer: '',
+                timestamp: ''
+            },
+            prompt4: {
+                question: "And how my best self would handle it is...",
+                answer: '',
+                timestamp: ''
+            },
+            prompt5: {
+                question: "If I was a high performance coach looking at myself from a high level, I would tell myself to remember that...",
+                answer: '',
+                timestamp: ''
+            },
+            prompt6: {
+                question: "I would know that today was a great success if at the end of that day I did or felt these things...",
+                answer: '',
+                timestamp: ''
+            }
+        },
+        answered: false
+    }
+
+    changeHandler = (answer, prompt) => {
+        const updatedPrompts = {
+            ...this.state.prompts
+        }
+        const updatedPrompt = {
+            ...updatedPrompts[prompt]
+        }
+        updatedPrompt.answer = answer.target.value;
+        updatedPrompts[prompt] = updatedPrompt;
+        this.setState({
+            prompts: updatedPrompts
+        })
+    }
+
+    submitAnswers = () => {
+        this.setState({
+            answered: true
+        })
+    }
+
+    render() {
+
+        const promptsArray = [];
+        for (let key in this.state.prompts) {
+            promptsArray.push({
+                id: key,
+                prompt: this.state.prompts[key]
+            })
+            console.log(promptsArray)
+        }
+
+        let form = (
+            <form onSubmit={this.submitAnswers}>
+                {promptsArray.map(promptElement => (
+                    <div key={promptElement.id}>
+                        <p>{promptElement.prompt.question}</p>
+                        <input
+                            value={promptElement.prompt.answer}
+                            onChange={(event) => this.changeHandler(event, promptElement.id)}
+                        />
+                    </div>
+                ))}
+                <button type="submit">Submit Answers</button>
+            </form>
+        )
+
+        const submittedAnswers = [];
+        for (let prompt in this.state.prompts) {
+            submittedAnswers.push({
+                id: prompt,
+                prompt: this.state.prompts[prompt]
+            })
+        }
+        let completedPrompts = (
+            <div>
+                {submittedAnswers.map(promptElement => (
+                    <div key={promptElement.id}>
+                        <p>{promptElement.prompt.question}</p>
+                        <p>{promptElement.prompt.answer}</p>
+                    </div>
+                ))}
+            </div>
+        )
+
+        return (
+            <div>
+                {this.state.answered ? completedPrompts : form}
+            </div>
+        );
+    }
+}
+
+export default Prompts;
